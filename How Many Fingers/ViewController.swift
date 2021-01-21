68597242ffb4ef292edec42146561029a364c349
alanrmendez <alan.mendez07@gmail.com>
2021-01-21 13:22:57 -0500

    Initial Commit

---
+++ How Many Fingers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift.
+//  How Many Fingers
+//
+//  Created by Alan Mendez on 1/21/21.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
